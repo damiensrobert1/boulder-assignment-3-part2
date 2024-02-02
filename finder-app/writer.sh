ac48f02f7460ed219eddcd597b865e60b27493fe
Damiens ROBERT <damiensrobert1@gmail.com>
2024-02-02 11:30:04 +0100

    use proper test in order to know if a variable is empty

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #Initialisation
-if [ -e $1 ]; then exit 1; else writefile=$1; writedir=$(dirname $writefile) ; fi
-if [ -e $2 ]; then exit 1; else writestr=$2; fi
+if [ -z $1 ]; then exit 1; else writefile=$1; writedir=$(dirname $writefile) ; fi
+if [ -z $2 ]; then exit 1; else writestr=$2; fi
 if [ ! -d $writedir ]; then  mkdir -p $writedir; fi
 
 #Running
